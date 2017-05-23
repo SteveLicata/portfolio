fe9f82edbc517dfc598cbbbf8200dfc5c75a28be
Steve Licata <Steve.Licata@gmail.com>
2017-05-23 16:52:12 -0400

    jquery added - animated header

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 window.onload = function() {
 
 
-var headerText = document.querySelector('.title');
+// var headerText = document.getElementById('title');
+//
+// headerText.style.display = "none";
 
-headerText.style.display = "none";
+//hide WEB DEVELOPER PORTFOLIO text
+$("#title").hide();
+//display WEB DEVELOPER PORTFOLIO text after 3 seconds
+$("#title").show(3000);
 
 
 }; //end window onload
